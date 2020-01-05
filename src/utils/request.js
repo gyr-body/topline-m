d014b4df7b37a61bfc05270199533d28bbab4974
gyr <guoyongyong-6@123.com>
2020-01-06 01:49:23 +0800

    封装 axios 请求函数

---
+++ src/utils/request.js
@@ -0,0 +1,30 @@
+// 封装axios 请求模块
+import axios from 'axios'
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
+})
+
+// 请求拦截器
+request.interceptors.request.use(
+  function (config) {
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
+// 响应拦截器
+request.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+  }
+)
+
+export default request

405ef75eaf0d5b38085807ebc6ef85336c8ddf12
gyr <guoyongyong-6@123.com>
2020-01-11 15:56:31 +0800

    Token处理--发送Token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,32 +1,42 @@
 // 封装axios 请求模块
 import axios from 'axios'
 import jsonBig from 'json-bigint'
+// 在非组件模块中直接加载获取容器
+// 这里拿到的 store 和你在组件中访问 this.$store 是一个东西
+import store from '@/store'
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
 })
 
 // 请求拦截器
-request.interceptors.request.use(
-  function (config) {
-    return config
-  },
-  function (error) {
-    return Promise.reject(error)
+request.interceptors.request.use(function (config) {
+  // config 请求配置对象，我们可以通过修改 config 来实现统一请求数据处理
+  const { user } = store.state
+
+  // 统一添加 token
+  if (user) {
+    // config.headers 获取操作请求头对象
+    // Authorization 是后端要求的字段名称
+    // 数据值后端要求提供：Bearer token数据
+    //    注意：Bearer 后面有个空格
+    // 老师，为啥？后端要求的
+    config.headers.Authorization = `Bearer ${user.token}`
   }
-)
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
 // 响应拦截器
-request.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error)
-  }
-)
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
 // axios 开放了自定义转换后返回数据的API
 // 处理后端返回数据超出js安全返回问题
 request.defaults.transformResponse = [function (data) {
